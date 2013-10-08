0e29866d3164a0edf344aaf98621ac3902fd9752
Georg Ledermann <mail@georg-ledermann.de>
2013-10-08 11:28:27 +0200

    Rails 3 compatibility

--- lib/keepr/chart.rb
+++ lib/keepr/chart.rb
@@ -12,9 +12,13 @@ module Keepr
       CSV.foreach(filename_with_path, :encoding => 'iso-8859-1', :col_sep => ';', :headers => true) do |row|
         number, name, kind = row[0], row[1], row[2]
 
-        Keepr::Account.find_or_create_by(number: number) do |account|
-          account.name = name
-          account.kind = kind
+        if Keepr::Account.respond_to?(:find_or_create_by!)
+          Keepr::Account.find_or_create_by!(number: number) do |account|
+            account.name = name
+            account.kind = kind
+          end
+        else
+          Keepr::Account.find_or_create_by_number(number, :name => name, :kind => kind)
         end
       end
     end
