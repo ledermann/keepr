72b2f9a79e584e145aec7281ea4f31efe8c097c4
Georg Ledermann <mail@georg-ledermann.de>
2014-10-02 10:23:24 +0200

    Tax: Added more validations

--- lib/keepr/tax.rb
+++ lib/keepr/tax.rb
@@ -1,7 +1,8 @@
 class Keepr::Tax < ActiveRecord::Base
   self.table_name = 'keepr_taxes'
 
-  validates_presence_of :name, :value
+  validates_presence_of :name, :value, :keepr_account_id
+  validates_numericality_of :value
 
   belongs_to :keepr_account, :class_name => 'Keepr::Account'
 
