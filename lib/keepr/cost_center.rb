930a9a1ab7eb174a5dcbdfd9f5c7099525485cdd
Georg Ledermann <mail@georg-ledermann.de>
2014-10-06 11:36:37 +0200

    CostCenter: Add missing validation

--- lib/keepr/cost_center.rb
+++ lib/keepr/cost_center.rb
@@ -2,6 +2,7 @@ class Keepr::CostCenter < ActiveRecord::Base
   self.table_name = 'keepr_cost_centers'
 
   validates_presence_of :number, :name
+  validates_uniqueness_of :number
 
   has_many :keepr_postings, :class_name => 'Keepr::Posting', :foreign_key => 'keepr_cost_center_id'
 end
