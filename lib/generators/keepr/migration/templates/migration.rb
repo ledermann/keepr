80c222919378e1e61741b5a40468513bde3f3937
Georg Ledermann <mail@georg-ledermann.de>
2016-04-27 13:35:18 +0200

    Migrations cleanup

--- lib/generators/keepr/migration/templates/migration.rb
+++ lib/generators/keepr/migration/templates/migration.rb
@@ -1,29 +1,31 @@
 class KeeprMigration < ActiveRecord::Migration
   def self.up
-    create_table :keepr_groups, force: true do |t|
+    create_table Keepr::Group, force: true do |t|
       t.integer    :target, :null => false
       t.string     :number
       t.string     :name, :null => false
       t.boolean    :is_result, :null => false, :default => false
       t.string     :ancestry
-    end
-    add_index :keepr_groups, :ancestry
 
-    create_table :keepr_taxes, force: true do |t|
+      t.index :ancestry
+    end
+
+    create_table Keepr::Tax, force: true do |t|
       t.string     :name, :null => false
       t.string     :description
       t.decimal    :value, :precision => 8, :scale => 2, :null => false
       t.references :keepr_account, :null => false
-    end
-    add_index :keepr_taxes, :keepr_account_id
 
-    create_table :keepr_cost_centers, force: true do |t|
+      t.index :keepr_account_id
+    end
+
+    create_table Keepr::CostCenter, force: true do |t|
       t.string     :number, :null => false
       t.string     :name, :null => false
       t.text       :note
     end
 
-    create_table :keepr_accounts, force: true do |t|
+    create_table Keepr::Account, force: true do |t|
       t.integer    :number, :null => false
       t.string     :ancestry
       t.string     :name, :null => false
@@ -33,14 +35,15 @@ class KeeprMigration < ActiveRecord::Migration
       t.references :keepr_tax
       t.datetime   :created_at
       t.datetime   :updated_at
-    end
-    add_index :keepr_accounts, :number
-    add_index :keepr_accounts, :ancestry
-    add_index :keepr_accounts, [:accountable_type, :accountable_id]
-    add_index :keepr_accounts, :keepr_group_id
-    add_index :keepr_accounts, :keepr_tax_id
 
-    create_table :keepr_journals, force: true do |t|
+      t.index :number
+      t.index :ancestry
+      t.index [:accountable_type, :accountable_id]
+      t.index :keepr_group_id
+      t.index :keepr_tax_id
+    end
+
+    create_table Keepr::Journal, force: true do |t|
       t.string   :number
       t.date     :date, :null => false
       t.string   :subject
@@ -49,27 +52,31 @@ class KeeprMigration < ActiveRecord::Migration
       t.boolean  :permanent, :null => false, :default => false
       t.datetime :created_at
       t.datetime :updated_at
-    end
-    add_index :keepr_journals, :date
-    add_index :keepr_journals, [:accountable_type, :accountable_id], :name => 'index_keepr_journals_on_accountable'
 
-    create_table :keepr_postings, force: true do |t|
+      t.index :date
+      t.index [:accountable_type, :accountable_id], :name => 'index_keepr_journals_on_accountable'
+    end
+
+    create_table Keepr::Posting, force: true do |t|
       t.references :keepr_account, :null => false
       t.references :keepr_journal, :null => false
       t.decimal    :amount, :precision => 8, :scale => 2, :null => false
       t.references :keepr_cost_center
       t.references :accountable, :polymorphic => true
+
+      t.index :keepr_account_id
+      t.index :keepr_journal_id
+      t.index :keepr_cost_center_id
+      t.index [:accountable_type, :accountable_id]
     end
-    add_index :keepr_postings, :keepr_account_id
-    add_index :keepr_postings, :keepr_journal_id
-    add_index :keepr_postings, :keepr_cost_center_id
-    add_index :keepr_postings, [:accountable_type, :accountable_id]
   end
 
   def self.down
-    drop_table :keepr_postings
-    drop_table :keepr_journals
-    drop_table :keepr_accounts
-    drop_table :keepr_groups
+    drop_table Keepr::Posting
+    drop_table Keepr::Journal
+    drop_table Keepr::Account
+    drop_table Keepr::CostCenter
+    drop_table Keepr::Tax
+    drop_table Keepr::Group
   end
 end
--- spec/support/spec_migration.rb
+++ spec/support/spec_migration.rb
@@ -1,20 +1,21 @@
 class SpecMigration < ActiveRecord::Migration
   def self.up
-    create_table :contacts, force: true do |t|
+    create_table Contact, force: true do |t|
       t.string :name
     end
 
-    create_table :ledgers, force: true do |t|
+    create_table Ledger, force: true do |t|
       t.string :bank_name
     end
 
-    create_table :documents, force: true do |t|
+    create_table Document, force: true do |t|
       t.string :number
     end
   end
 
   def self.down
-    drop_table :documents
-    drop_table :contacts
+    drop_table Document
+    drop_table Ledger
+    drop_table Contact
   end
 end
