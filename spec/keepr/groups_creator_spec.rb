5fc9758fe3a2f3704b46e0e4e016abe44d8b0e56
Edgardo Martin Gerez <edmargerez@gmail.com>
2020-10-07 18:38:36 -0300

    added groups creation files for internationalization

--- spec/keepr/groups_creator_spec.rb
+++ spec/keepr/groups_creator_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe Keepr::GroupsCreator do
-  context 'balance groups' do
+  context 'balance groups in german' do
     before :each do
-      Keepr::GroupsCreator.new(:balance).run
+      Keepr::GroupsCreator.new(:balance, :de).run
     end
 
     it 'should create groups' do
@@ -13,8 +13,8 @@ describe Keepr::GroupsCreator do
       expect(Keepr::Group.asset.count).to eq(36)
       expect(Keepr::Group.liability.count).to eq(28)
 
-      compare_with_source(Keepr::Group.asset, 'asset.txt')
-      compare_with_source(Keepr::Group.liability, 'liability.txt')
+      compare_with_source(Keepr::Group.asset, 'de', 'asset.txt')
+      compare_with_source(Keepr::Group.liability, 'de', 'liability.txt')
     end
 
     it 'should create result group' do
@@ -24,21 +24,21 @@ describe Keepr::GroupsCreator do
 
   context 'profit & loss groups' do
     before :each do
-      Keepr::GroupsCreator.new(:profit_and_loss).run
+      Keepr::GroupsCreator.new(:profit_and_loss, :de).run
     end
 
     it 'should create profit & loss groups' do
       expect(Keepr::Group.count).to eq(31)
       expect(Keepr::Group.profit_and_loss.count).to eq(31)
 
-      compare_with_source(Keepr::Group.profit_and_loss, 'profit_and_loss.txt')
+      compare_with_source(Keepr::Group.profit_and_loss, 'de', 'profit_and_loss.txt')
     end
   end
 
   private
 
-  def compare_with_source(scope, filename)
-    full_filename = File.join(File.dirname(__FILE__), "../../lib/keepr/groups_creator/#{filename}")
+  def compare_with_source(scope, language, filename)
+    full_filename = File.join(File.dirname(__FILE__), "../../lib/keepr/groups_creator/#{language}/#{filename}")
     source = File.read(full_filename)
 
     lines = scope.find_each.map { |g| "#{' ' * g.depth * 2}#{g.number} #{g.name}\n" }.join
