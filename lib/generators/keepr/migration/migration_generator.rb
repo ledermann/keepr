2841cca702970bddad2ed8b75cd9f8ce60b76374
Simon Rentzke <simon@rentzke.com>
2023-12-20 16:01:23 +1000

    Fix migration_generator.rb
    This fixes a compatibility bug with Rails 7.1.2. 
    
    `ActiveRecord::Base.timestamped_migrations` is not a valid method, but ` ActiveRecord.timestamped_migrations` is.
    
    https://www.rubydoc.info/docs/rails/ActiveRecord:timestamped_migrations

--- lib/generators/keepr/migration/migration_generator.rb
+++ lib/generators/keepr/migration/migration_generator.rb
@@ -15,7 +15,7 @@ module Keepr
     end
 
     def self.next_migration_number(dirname)
-      if ActiveRecord::Base.timestamped_migrations
+      if ActiveRecord.timestamped_migrations
         Time.now.utc.strftime('%Y%m%d%H%M%S')
       else
         format('%.3d', (current_migration_number(dirname) + 1))
